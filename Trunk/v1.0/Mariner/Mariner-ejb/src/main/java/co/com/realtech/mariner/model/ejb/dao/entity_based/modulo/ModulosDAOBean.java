a106cb6d05972f659819d9946b50d9d4aea50a3f
fabianagudelo <fabianagudelo@244022c9-da74-4f00-9af4-273f6a5020fb>
2016-05-17 23:19:58 +0000

    Paso a Oracle Completo

--- Trunk/v1.0/Mariner/Mariner-ejb/src/main/java/co/com/realtech/mariner/model/ejb/dao/entity_based/modulo/ModulosDAOBean.java
+++ Trunk/v1.0/Mariner/Mariner-ejb/src/main/java/co/com/realtech/mariner/model/ejb/dao/entity_based/modulo/ModulosDAOBean.java
@@ -36,33 +36,24 @@ public class ModulosDAOBean extends GenericDAOBean implements ModulosDAOBeanLoca
         List<MarModulos> modulos = new ArrayList<>();
         ModelTreeUtil modelTree = new ModelTreeUtil();
         try {
-            String query = "WITH RECURSIVE tree \n"
-                    + "AS \n"
-                    + "(\n"
-                    + "    SELECT \n"
-                    + "        mod_id, mod_id_padre, mod_id||'' AS path, mod_orden \n"
-                    + "    FROM mar_modulos WHERE mod_id_padre IS NULL\n"
-                    + "    UNION\n"
-                    + "    SELECT \n"
-                    + "       f1.mod_id, f1.mod_id_padre, tree.path || '-' || f1.mod_id AS path,f1.mod_orden \n"
-                    + "    FROM \n"
-                    + "        tree \n"
-                    + "    JOIN mar_modulos f1 ON f1.mod_id_padre = tree.mod_id\n"
-                    + ") \n"
-                    + "SELECT path FROM tree WHERE mod_id in (\n"
-                    + "	SELECT m.mod_id FROM mar_modulos m \n"
-                    + "    INNER JOIN mar_roles_modulos ro ON ro.mod_id=m.mod_id\n"
-                    + "    INNER JOIN mar_roles r ON r.rol_id=ro.rol_id\n"
-                    + "    INNER JOIN mar_roles_usuarios ru ON ru.rol_id=r.rol_id WHERE ru.usu_id=:usuId ORDER BY m.mod_nombre ASC\n"
-                    + ") ORDER BY mod_orden NULLS LAST";
+            String query = "SELECT SYS_CONNECT_BY_PATH(MOD_ID, '-') PATH\n" +
+                            "FROM mar_modulos WHERE MOD_ID IN(\n" +
+                            "   SELECT RP.MOD_ID\n" +
+                            "   FROM mar_roles_usuarios UR\n" +
+                            "   INNER JOIN mar_roles_modulos RP ON UR.ROL_ID=RP.ROL_ID\n" +
+                            "       WHERE UR.usu_id=:usuId)\n" +
+                            "START WITH MOD_ID_PADRE IS NULL\n" +
+                            "CONNECT BY PRIOR MOD_ID=MOD_ID_PADRE";
             Query q = getEntityManager().createNativeQuery(query);
             q.setParameter("usuId", usuario.getUsuId());
             List<String> paths = (List<String>) q.getResultList();
-
             paths.stream().map((path) -> path.split("-")).forEach((elements) -> {
                 for (String element : elements) {
-                    MarModulos singleMod = getEntityManager().find(MarModulos.class, new BigDecimal(element));
-                    modelTree.walkArray(modulos, singleMod, 0);
+                    if (!element.isEmpty()) {
+                        System.out.println("element = " + element);
+                        MarModulos singleMod = getEntityManager().find(MarModulos.class, new BigDecimal(element));
+                        modelTree.walkArray(modulos, singleMod, 0);
+                    }
                 }
             });
         } catch (Exception e) {
