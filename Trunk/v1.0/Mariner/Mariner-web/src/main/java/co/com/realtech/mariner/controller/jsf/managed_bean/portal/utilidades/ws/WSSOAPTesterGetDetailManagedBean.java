b7d22c376cc7c03c34760ff29c64be08110b0fab
nemthys <nemthys@244022c9-da74-4f00-9af4-273f6a5020fb>
2016-05-24 15:13:42 +0000

    Ajustes capa de servicios

--- Trunk/v1.0/Mariner/Mariner-web/src/main/java/co/com/realtech/mariner/controller/jsf/managed_bean/portal/utilidades/ws/WSSOAPTesterGetDetailManagedBean.java
+++ Trunk/v1.0/Mariner/Mariner-web/src/main/java/co/com/realtech/mariner/controller/jsf/managed_bean/portal/utilidades/ws/WSSOAPTesterGetDetailManagedBean.java
@@ -2,15 +2,13 @@ package co.com.realtech.mariner.controller.jsf.managed_bean.portal.utilidades.ws
 
 import co.com.realtech.mariner.controller.jsf.managed_bean.main.GenericManagedBean;
 import co.com.realtech.mariner.model.ejb.ws.sap.WSSAPConsumerBeanLocal;
-import co.com.realtech.mariner.model.ejb.ws.sap.mappers.get_detail_method.ZPSCDPRNCAB;
-import co.com.realtech.mariner.model.ejb.ws.sap.mappers.get_detail_method.ZPSCDTTVURDETAIL;
+import co.com.realtech.mariner.model.ejb.ws.sap.mappers.business.get_detail_method.DetalleLiquidacion;
 import co.com.realtech.mariner.util.primefaces.dialogos.Effects;
 import co.com.realtech.mariner.util.primefaces.dialogos.PrimeFacesPopup;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.xml.ws.Holder;
 
 /**
  * Controlador JSF manejo de utilidades consumo de WS SAP
@@ -42,10 +40,9 @@ public class WSSOAPTesterGetDetailManagedBean extends GenericManagedBean impleme
      */
     public void ejecutarMetodo() {
         try {
-            Holder<ZPSCDPRNCAB> responseHeader = new Holder<>();
-            Holder<ZPSCDTTVURDETAIL> responseDetail = new Holder<>();
-            wSSAPConsumerBean.getDetail(getCodigoLiquidacion(), responseHeader, responseDetail);
-            PrimeFacesPopup.lanzarDialog(Effects.Slide, "Notificacion", "Servicio web ejecutado correctamente, parametro:"+responseHeader.value.getVFECLIQ(), true, false);
+            DetalleLiquidacion detalle;
+            detalle = wSSAPConsumerBean.getDetail(getCodigoLiquidacion());
+            PrimeFacesPopup.lanzarDialog(Effects.Slide, "Notificacion", "Servicio web ejecutado correctamente, parametro:" + detalle.getMatricula(), true, false);
         } catch (Exception e) {
             PrimeFacesPopup.lanzarDialog(Effects.Explode, "Error", "Error consultando meotodo getDetail, causado por " + e, true, false);
             logger.error("Error ejecutando metodo get Details, causado por " + e);
