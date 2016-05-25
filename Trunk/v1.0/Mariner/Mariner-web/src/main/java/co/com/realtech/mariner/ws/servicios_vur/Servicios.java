2a24b77512e596617c8b8e3419fbe881f6af579e
nemthys <nemthys@244022c9-da74-4f00-9af4-273f6a5020fb>
2016-05-25 17:12:38 +0000

    FuckYea cambio de servicios web y LogicalMapper

--- Trunk/v1.0/Mariner/Mariner-web/src/main/java/co/com/realtech/mariner/ws/servicios_vur/Servicios.java
+++ Trunk/v1.0/Mariner/Mariner-web/src/main/java/co/com/realtech/mariner/ws/servicios_vur/Servicios.java
@@ -1,22 +1,11 @@
 package co.com.realtech.mariner.ws.servicios_vur;
 
-import co.com.realtech.mariner.model.ejb.dao.entity_based.radicaciones.RadicFasesEstadosDAOBeanLocal;
 import co.com.realtech.mariner.model.ejb.dao.generic.GenericDAOBeanLocal;
-import co.com.realtech.mariner.model.ejb.ws.sap.WSSAPConsumerBeanLocal;
-import co.com.realtech.mariner.model.ejb.ws.sap.mappers.business.get_detail_method.DetalleLiquidacion;
 import co.com.realtech.mariner.model.entity.MarRadicaciones;
-import co.com.realtech.mariner.model.entity.MarRadicacionesActosSap;
-import co.com.realtech.mariner.model.entity.MarRadicacionesDetallesSap;
-import co.com.realtech.mariner.model.entity.MarRadicacionesFasesEstados;
-import co.com.realtech.mariner.model.entity.MarUsuarios;
+import co.com.realtech.mariner.model.logic.radicaciones_sap.SAPRadicacionesLogicOperations;
 import co.com.realtech.mariner.model.sdo.estandar.EntidadLiquidacionResultado;
 import co.com.realtech.mariner.model.sdo.logs.EntidadLog;
-import co.com.realtech.mariner.util.string.BusinessStringUtils;
-import co.com.realtech.mariner.ws.servicios_vur.marshall.liquidacion.LiquidacionMarshaller;
-import co.com.realtech.mariner.ws.servicios_vur.marshall.liquidacion.LiquidacionMarshallersBuilder;
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
 import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -35,12 +24,6 @@ public class Servicios {
     @EJB(beanName = "GenericDAOBean")
     protected GenericDAOBeanLocal genericDAOBean;
 
-    @EJB(beanName = "RadicFasesEstadosDAOBean")
-    private RadicFasesEstadosDAOBeanLocal radicFasesEstadosDAOBean;
-
-    @EJB(beanName = "WSSAPConsumerBean")
-    private WSSAPConsumerBeanLocal wSSAPConsumerBean;
-
     /**
      * Metodo expuerto para recibir notificacion de cuando se crea la
      * liquidacion en otros sistemas.
@@ -55,66 +38,8 @@ public class Servicios {
         try {
             // consulta detalle.
             MarRadicaciones radicacion = (MarRadicaciones) genericDAOBean.findByColumn(MarRadicaciones.class, "radId", new BigDecimal(codigoRadicacion.toString()));
-
-            if (radicacion != null) {
-                // Verificamos que la radiacion este en estado Pendiente SAP G-S
-                MarRadicacionesFasesEstados estado = radicFasesEstadosDAOBean.obtenerUltimaFaseDeRadicacion(radicacion);
-
-                if (estado.getFesId().getFesCodigo().equals("G-S")) {
-                    MarRadicacionesDetallesSap detalleSAP = (MarRadicacionesDetallesSap) genericDAOBean.findByColumn(MarRadicacionesDetallesSap.class, "radId", radicacion);
-                    if (detalleSAP == null) {
-                        detalleSAP = new MarRadicacionesDetallesSap();
-                        detalleSAP.setRadId(radicacion);
-                    }
-                    // Buscar la radicacion por liquidacion en SAP
-                    codigoLiquidacion=BusinessStringUtils.convertNumeroLiquidacion(codigoLiquidacion);
-                    DetalleLiquidacion detalleWS = wSSAPConsumerBean.getDetail(codigoLiquidacion);
-
-                    if (detalleWS.getLiqNumero() != null && !detalleWS.getLiqNumero().equals("")) {
-
-                        LiquidacionMarshaller marshall = LiquidacionMarshallersBuilder.createLiquidacionMarshaller();
-                        marshall.fillLiquidacionValues(detalleSAP, radicacion, detalleWS);
-                        detalleSAP = marshall.getSap();
-                        List<MarRadicacionesActosSap> actosSAP = marshall.getActos();
-                        // Guardamos la radicacion.
-                        genericDAOBean.merge(radicacion);
-                        
-                        // Guardamos informacion de la liquidacion.
-                        if (detalleSAP.getRdeId() == null) {
-                            genericDAOBean.save(detalleSAP);
-                        } else {
-                            genericDAOBean.merge(detalleSAP);
-                        }
-                        // Guardamos los actos de la liquidacion
-                        for (MarRadicacionesActosSap actoSap : actosSAP) {
-                            actoSap.setRdeId(detalleSAP);
-                            actoSap.setAudFecha(new Date());
-                            genericDAOBean.save(actoSap);
-                        }                        
-                        // Guardamos la nueva fase estado del proceso.
-                        MarUsuarios usuarioSistema = ((MarUsuarios) genericDAOBean.findByColumn(MarUsuarios.class, "usuLogin", "MARINER"));
-                        BigDecimal regEstado = (BigDecimal) genericDAOBean.callGenericFunction("PKG_VUR_CORE.fn_ingresar_fase_estado", radicacion, "G-A", "A", usuarioSistema, "", null);
-
-                        if (regEstado.intValue() != -999) {
-                            salida.setEstado("OK");
-                            salida.setCodigoConfirmacion(regEstado.longValue());
-                            salida.setLog(new EntidadLog("1000", "OK", "Proceso de liquidacion recibida en la plataforma", "El proceso de liquidacion No." + codigoLiquidacion + " ha sido recibido por la plataforma."));
-                        } else {
-                            salida.setEstado("ERROR");
-                            salida.setLog(new EntidadLog("1005", "ERROR", "Error registrando estado Fase en sistema", "Se obtubieron los datos de SAP, pero no fue posible vincular el Estado Fase G-A"));
-                        }
-                    } else {
-                        salida.setEstado("ERROR");
-                        salida.setLog(new EntidadLog("1004", "ERROR", "Liquidacion no encontrada en SAP", "Error, la liquidacion " + codigoLiquidacion + " no ha sido retornadada por el sistema SAP"));
-                    }
-                } else {
-                    salida.setEstado("ERROR");
-                    salida.setLog(new EntidadLog("1006", "ERROR", "Error validacion en Fase G-S", "La radicacion seleccionada no se encuentra en estado G-S para pasar a G-A"));
-                }
-            } else {
-                salida.setEstado("ERROR");
-                salida.setLog(new EntidadLog("1002", "ERROR", "No se ha encontrado la Radicacion con el codigo ingresado", "Error buscando radicacion " + codigoRadicacion + " no se ha encontrado el codigo de radicacion en el sistema."));
-            }
+            SAPRadicacionesLogicOperations sapLogic = SAPRadicacionesLogicOperations.create();
+            salida = sapLogic.vincularRadicacionSAP(radicacion, codigoLiquidacion);
         } catch (Exception e) {
             salida.setEstado("ERROR");
             salida.setLog(new EntidadLog("1001", "ERROR", "Error interno procesando peticion", "Error interno causado por " + e));
